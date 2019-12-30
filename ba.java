845aa6d6dc53c735b3105ff546ca6d2442b554cb
1413 <1413097437@qq.com>
2019-12-30 17:32:18 +0800

    aa

---
+++ ba.java
@@ -0,0 +1,11 @@
+public class ba{
+	public static void main(String args[]){
+		System.out.println("´òÓ¡¾ØÐÎ:");
+		for (int i = 1;i <= 5  ;i++ ){
+			for (int j = 1;j<= 5 ;j++ ){
+				System.out.print("*");
+			}
+					System.out.println("");
+		}
+	}
+}
